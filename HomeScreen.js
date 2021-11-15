b44aa22b163a57445058d445fb43880a7e8f7f89
PedroEsquerra <38469037+pedroesquerra@users.noreply.github.com>
2021-11-15 12:09:06 -0800

    Screens

---
+++ HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Button 
+                title="Vamos a la pantalla detalle" 
+                onPress={()=>navigation.navigate('DetailsScreen',{nombre:'Pedro'})}
+            />
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+});
